6001475533a7e9a220aa1048ef490c723aa728f6
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-02-08 12:02:46 +0700

    08/02/2023

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routes/web');
+const mysql = require('mysql2');
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -10,6 +11,24 @@ const hostname = process.env.HOST_NAME;
 
 //Config template engine
 configViewEngine(app);
+
+//Kết nối CSDL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'velkoz',
+})
+
+connection.query(
+    'SELECT * FROM TUONG u',
+    function (err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+
+)
 
 //Khai báo route
 app.use('/', webRoute);
